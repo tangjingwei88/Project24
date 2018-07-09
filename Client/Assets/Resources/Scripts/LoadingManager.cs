c3bb2707f2d1a14723b6199d5daf5052f2a8ce4d
tangjingwei88 <jingweitang88@gmail.com>
2018-07-09 19:14:33 +0800

    loadingmanager

--- Client/Assets/Resources/Scripts/LoadingManager.cs
+++ Client/Assets/Resources/Scripts/LoadingManager.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class LoadingManager : MonoBehaviour {
 
+
+    #region 变量
+    private bool loadingFinished = false;
+
+    public GameObject mainSceneGO;
+    #endregion
+
     private static LoadingManager _instance;
     public static LoadingManager Instance {
         get {
@@ -21,4 +28,37 @@ public class LoadingManager : MonoBehaviour {
     {
 
     }
+
+
+    #region 方法
+
+    /// <summary>
+    /// 异步场景加载
+    /// </summary>
+    /// <param name="sceneName"></param>
+    /// <returns></returns>
+    IEnumerator WaitLoadingTargetScene(string sceneName)
+    {
+        AsyncOperation async = Application.LoadLevelAsync(sceneName);
+        async.priority = 0;
+        yield return async;
+
+        loadingFinished = true;
+    }
+
+
+    private void AfterLoadMainScene()
+    {
+        if (mainSceneGO == null)
+        {
+            mainSceneGO = GameObject.Find("UIMain");
+        }
+
+        if (mainSceneGO != null)
+        {
+            mainSceneGO.SetActive(true);
+        }
+    }
+    #endregion
+
 }
