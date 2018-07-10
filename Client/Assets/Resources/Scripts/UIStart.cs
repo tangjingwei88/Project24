7d00d24c8dc996f76e3f12ec7bfc1a2db1a6d170
tangjingwei88 <jingweitang88@gmail.com>
2018-07-10 19:31:21 +0800

    uistart添加

--- Client/Assets/Resources/Scripts/UIStart.cs
+++ Client/Assets/Resources/Scripts/UIStart.cs
@@ -3,9 +3,40 @@ using System.Collections;
 
 public class UIStart : MonoBehaviour {
 
-    
-	void Start () {
+    #region 引用
 
+    public GameObject theLoginPartPanel;
+    public GameObject theRegisterPanel;
+
+    #endregion
+
+    #region 属性
+    private static UIStart _instance;
+    public static UIStart Instance {
+        get {
+            return _instance;
+        }
+    }
+
+    #endregion
+
+
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+
+
+    void Start () {
+        theLoginPartPanel.gameObject.SetActive(true);
 	}
 
+
+    #region 方法
+
+
+
+    #endregion
 }
